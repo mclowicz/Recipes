5e434124f471b70cc03f6df03c2dd71496b3e7bd
mclowicz <87059929+mclowicz@users.noreply.github.com>
2022-04-23 12:09:27 +0200

    delete build dir

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_mclowicz_App.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.mclowicz.App"
-)
-public final class _com_mclowicz_App {
-}
